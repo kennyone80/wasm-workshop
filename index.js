4a15889c83b154b52eb93a92597e828d56011708
m4dz <code@m4dz.net>
2019-10-11 15:10:03 +0200

    Dynamic hello world

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import init, { greet } from './lib/pkg/wasm_workshop.js'
 
 init().then(() => {
   document.querySelector('button').addEventListener('click', e => {
-    greet()
+    let name = document.querySelector('input').value
+    greet(name)
   })
 })
