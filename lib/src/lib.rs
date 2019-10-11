ecfb97b30ae0082d2f30d7ca8e593d88e4799fb2
m4dz <code@m4dz.net>
2019-10-12 00:03:19 +0200

    Update state

--- lib/src/lib.rs
+++ lib/src/lib.rs
@@ -8,8 +8,33 @@ use wasm_bindgen::prelude::*;
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
+#[macro_use]
+extern crate serde_derive;
+
+#[derive(Serialize, Deserialize)]
+pub struct Node {
+    id: String,
+    parent: String,
+    nonce: String,
+    records: Vec<Record>
+}
+#[derive(Serialize, Deserialize)]
+pub struct Record {
+    id: String,
+    email: String,
+    ip_address: String,
+    last_name: String,
+    first_name: String,
+    password: String,
+    desc: String,
+}
 #[wasm_bindgen]
 pub struct Stats(pub f64, pub usize);
+
+#[wasm_bindgen(module = "/bind.js")]
+extern "C" {
+    fn updateState(state: JsValue);
+}
 
 #[wasm_bindgen]
 pub fn compute(nodes: &JsValue, limit: usize) -> Stats {
@@ -19,5 +44,6 @@ pub fn compute(nodes: &JsValue, limit: usize) -> Stats {
         .performance()
         .expect("performance should be available");
     let t = performance.now();
+    updateState(JsValue::from_serde(&nodes).unwrap());
     Stats(performance.now() - t, rounds)
 }
